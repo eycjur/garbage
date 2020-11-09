2f5e016ddec7e63907a2b307ae3b8bb5e0d69fff
eycjur <63308909+eycjur@users.noreply.github.com>
2020-11-09 20:46:30 +0900

    モデル出力

--- model.py
+++ model.py
@@ -29,13 +29,13 @@ train_datagen = ImageDataGenerator(
     rotation_range=90,
     width_shift_range=0.1,
     height_shift_range=0.1,
-    rescale=1.0 / 255,
+    rescale=1. / 255,
     zoom_range=0.2,
     horizontal_flip=True,
     vertical_flip=True
 )
 
-val_datagen = ImageDataGenerator(rescale=1.0 / 255)
+val_datagen = ImageDataGenerator(rescale=1. / 255)
 
 # ジェネレーターを生成
 train_generator = train_datagen.flow_from_directory(
@@ -45,7 +45,7 @@ train_generator = train_datagen.flow_from_directory(
     classes=classes,
     class_mode='categorical',
     batch_size=batch_size,
-    shuffle=True)
+    shuffle=True).repeat()
 
 val_generator = val_datagen.flow_from_directory(
     val_dir,
@@ -81,9 +81,9 @@ vgg_model.compile(loss='categorical_crossentropy',
           metrics=['acc'])
 vgg_model.summary()
 
-history = vgg_model.fit(
+history = vgg_model.fit_generator(
     train_generator,
-    steps_per_epoch=len(train_generator),
+    steps_per_epoch=len(train_generator)+1,
     epochs=30,
     validation_data=val_generator,
     validation_steps=len(val_generator))
@@ -95,7 +95,7 @@ plt.plot(history.history["val_acc"], label="val_acc", ls="-", marker="x")
 plt.ylabel("acc")
 plt.xlabel("epoch")
 plt.legend(loc="best")
-plt.savefig("acc")
+# plt.savefig("acc")
 plt.close()
 
 plt.plot(history.history["loss"], label="loss", ls="-", marker="o")
@@ -103,12 +103,12 @@ plt.plot(history.history["val_loss"], label="val_loss", ls="-", marker="x")
 plt.ylabel("loss")
 plt.xlabel("epoch")
 plt.legend(loc="best")
-plt.savefig("loss")
+# plt.savefig("loss")
 plt.close()
 
 # 保存
-open("model.json", 'w').write(vgg_model.to_json())
-vgg_model.save_weights('param.hdf5')
+# open("model.json", 'w').write(vgg_model.to_json())
+# vgg_model.save_weights('param.hdf5')
 
 
 
@@ -146,7 +146,7 @@ model.load_weights('param.hdf5')
 img_width, img_height = 150, 150
 classes = ['不燃ごみ', '包装容器プラスチック類', '可燃ごみ', '有害ごみ', '資源品']
 
-filename = "val/資源品/IMG_20201108_105804.jpg"
+filename = "val/不燃ごみ/IMG_20201108_114503.jpg"
 img = image.load_img(filename, target_size=(img_height, img_width))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
@@ -154,15 +154,14 @@ x = np.expand_dims(x, axis=0)
 # これを忘れると結果がおかしくなるので注意
 x = x / 255.0
 # 表示
-plt.imshow(img)
+plt.imshow(img.resize((120,160)))
 plt.show()
 plt.close()
-# 指数表記を禁止にする
-np.set_printoptions(suppress=True)
 
 # 画像の人物を予測
 pred = model.predict(x)[0]
 # 結果を表示する
 result = {c:s for (c, s) in zip(classes, pred*100)}
+result = sorted(result.items(), key=lambda x:x[1], reverse=True)
 print(result)
-
+result
