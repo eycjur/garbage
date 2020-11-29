c5e530618eb480e5fc62dfb1ed7a1657d2af1aaa
eycjur <63308909+eycjur@users.noreply.github.com>
2020-11-30 07:33:04 +0900

    end script

--- garbage_proj/settings.py
+++ garbage_proj/settings.py
@@ -78,16 +78,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'garbage_proj.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -141,6 +131,15 @@ try:
 except:
     print("except")
 
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
