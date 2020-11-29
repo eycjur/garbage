ab042ee21cffff8399f5324fc73e6155da8e3f36
eycjur <63308909+eycjur@users.noreply.github.com>
2020-11-30 08:22:56 +0900

    base64

--- garbage/views.py
+++ garbage/views.py
@@ -67,12 +67,24 @@ def result(request, num=0):
             return index(None)
 
     pred = predict(img)
+    base64Img = get_base64(img)
     params = {
         "img":img,
         "pred":pred,
-        "serch_form":SerchClassForm()
+        "serch_form":SerchClassForm(),
+        "base64Img":base64Img
     }
     return render(request, "garbage/result.html", params)
+
+
+def get_base64(img):
+    import base64
+    import io
+    draw = Image.open(img)
+    buffer = io.BytesIO()
+    draw.save(buffer, format="PNG")
+    base64Img = base64.b64encode(buffer.getvalue()).decode()
+    return base64Img
 
 
 def search(request):
@@ -113,18 +125,15 @@ def predict(img):
     """予測モデル"""
     # 読み込み
     import numpy as np
-    import matplotlib.pyplot as plt
     from keras.preprocessing import image
     from keras.models import model_from_json
-    from PIL import Image
 
     model = model_from_json(open(BASE_DIR + "/model/model.json").read())
     model.load_weights(BASE_DIR + '/model/param.hdf5')
 
     img_width, img_height = 150, 150
     img = Image.open(img)
-    img.save(BASE_DIR + "/static/garbage/media/images/image.jpg")
-    img.save(BASE_DIR + "/staticfiles/garbage/media/images/image.jpg")
+    # img.save(BASE_DIR + "/static/garbage/media/images/image.jpg")
     img = np.array(img.resize((img_width, img_height)))
     classes = ['不燃ごみ', '包装容器プラスチック類', '可燃ごみ', '有害ごみ', '資源品']
     days = ["第2・4木曜日", "水曜日", "火・金曜日", "第1・3金曜日", "第1・3金曜日"]
