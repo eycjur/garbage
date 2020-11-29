b99908dde0d461191602e02aa709cfad41fff6ef
eycjur <63308909+eycjur@users.noreply.github.com>
2020-11-29 16:48:08 +0900

    フォームの作成

--- garbage/forms.py
+++ garbage/forms.py
@@ -4,3 +4,7 @@ class UploadPictureForm(forms.Form):
 
 class SerchClassForm(forms.Form):
 	word = forms.CharField(max_length=100, label="検索ワード")
+
+class InquiryForm(forms.Form):
+	title = forms.CharField(max_length=100, label="タイトル")
+	text = forms.CharField(max_length=100, label="内容")
