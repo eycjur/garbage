e4e77c6c434fee798773a87ca59e0081520fee67
eycjur <63308909+eycjur@users.noreply.github.com>
2021-02-26 07:57:25 +0900

    [add]モデル処理のテストを追加
    サンプル画像クリック時の共同をテスト

--- garbage/tests/test_views.py
+++ garbage/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 
 class TestClickSample(LiveServerTestCase):
@@ -19,7 +20,13 @@ class TestClickSample(LiveServerTestCase):
         self.selenium.get('http://localhost:8000/garbage')
         self.assertEquals("画像でゴミ分類！", self.selenium.title)
 
+    def test_click_sample(self):
+        """モデルの動きが正常かを確認
+        サンプル画像をクリックした際の挙動を確認
+        """
+        self.selenium.get('http://localhost:8000/garbage')
+        self.selenium.find_elements_by_class_name("sample-img")[1].click()
 
-
-
+        num_tr = len(self.selenium.find_elements(By.XPATH, '//tr'))
+        self.assertEquals(6, num_tr)
 
