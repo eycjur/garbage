6e73674849063c7741872d48d35c060bdf3ac0e8
eycjur <63308909+eycjur@users.noreply.github.com>
2021-02-26 08:13:52 +0900

    [add]文字列検索時のテストを追加
    検索ボックス利用時のテストを追加

--- garbage/tests/test_views.py
+++ garbage/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class TestClickSample(LiveServerTestCase):
@@ -30,3 +31,13 @@ class TestClickSample(LiveServerTestCase):
         num_tr = len(self.selenium.find_elements(By.XPATH, '//tr'))
         self.assertEquals(6, num_tr)
 
+    def test_search(self):
+        """文字検索時の挙動をテスト
+        """
+        self.selenium.get('http://localhost:8000/garbage')
+        search_box = self.selenium.find_elements(By.XPATH, '//input[@name="word"]')[1]
+        search_box.send_keys('針')
+        search_box.send_keys(Keys.ENTER)
+
+        num_tr = len(self.selenium.find_elements(By.XPATH, '//tr'))
+        self.assertEquals(14, num_tr)
